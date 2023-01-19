7eaafdb925afbfd7e8d1772ee9e05763b245648d
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2023-01-19 15:18:22 +0100

    Fixed AI launch script

--- scripts/run_ai_module.sh
+++ scripts/run_ai_module.sh
@@ -15,5 +15,5 @@ source /home/edgecar_msgs/catkin_ws/devel/setup.bash
 source /home/aidrivingmodule/catkin_ws/devel/setup.bash --extend
 
 #roslaunch detector_node detector_node.launch veh:=$VEHICLE_NAME --wait
-#roslaunch driving_module driving_module.launch veh:=$VEHICLE_NAME --wait
-roslaunch autopilot autopilot.launch veh:=$VEHICLE_NAME --wait
+#roslaunch autopilot autopilot.launch veh:=$VEHICLE_NAME --wait
+roslaunch driving_module driving_module.launch veh:=$VEHICLE_NAME --wait
